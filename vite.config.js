4c1465db0102ab2e4fb30dff33e2174953d9802f
Specter <ishanmaiti1234@gmail.com>
2026-06-03 17:33:34 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  base: '/Portfolio3.0/',
   plugins: [
     react(),
     tailwindcss(),
